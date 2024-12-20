2c532f4d02b7199dd58f921324c339994a39c035
oinqu <stanislav@readyplayer.me>
2024-12-20 21:13:16 +0200

    lint

--- common/json_format.ts
+++ common/json_format.ts
@@ -2,27 +2,27 @@ import { KBN_FIELD_TYPES } from '@kbn/field-types';
 import { FieldFormat, FieldFormatParams, type HtmlContextTypeConvert } from '@kbn/field-formats-plugin/common';
 
 export class JsonFormat extends FieldFormat {
-	static id = 'json';
-	static title = 'JSON';
-	static fieldType = KBN_FIELD_TYPES.STRING;
+    static id = 'json';
+    static title = 'JSON';
+    static fieldType = KBN_FIELD_TYPES.STRING;
 
-  getParamDefaults(): FieldFormatParams {
-		return {}
-	}
+    getParamDefaults(): FieldFormatParams {
+        return {}
+    }
 
-  htmlConvert: HtmlContextTypeConvert = (val) => {
-		const { isJson, value } = getFormattedJson(String(val))
-    if(!isJson) {
-			return value
-		}
-		return `<pre>${value}</pre>`
-	}
+    htmlConvert: HtmlContextTypeConvert = (val) => {
+        const { isJson, value } = getFormattedJson(String(val))
+        if(!isJson) {
+            return value
+        }
+        return `<pre>${value}</pre>`
+    }
 }
 
 function getFormattedJson(value: string) {
-  try {
-    return { isJson: true, value: JSON.stringify(JSON.parse(value), null, 2) };
-  } catch {
-    return { isJson: false, value };
-  }
+    try {
+        return { isJson: true, value: JSON.stringify(JSON.parse(value), null, 2) };
+    } catch {
+        return { isJson: false, value };
+    }
 }
